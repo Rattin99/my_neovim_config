26ab4220e3773d10991bd2531fa9a9e7d61b5755
Rattin99 <rattin99m18@gmail.com>
2024-10-08 20:49:13 +0600

    last stable version

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,5 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
-vim.api.nvim_set_keymap("n", "<leaer>tt", ":Toggleterm<CR>", { noremap = true, silent = true })
+-- Toggle the terminal with <leader>tt
+vim.api.nvim_set_keymap("n", "<leader>tt", ":ToggleTerm<CR>", { noremap = true, silent = true })
+
+-- Open a terminal in a split window with <leader>t
 vim.api.nvim_set_keymap("n", "<leader>t", ":botright split | terminal<CR>", { noremap = true, silent = true })
